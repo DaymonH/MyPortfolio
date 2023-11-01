2190f15e32d1fc6d6e1673e75f7cb52949adc967
Daymon Hansen <83380479+DaymonH@users.noreply.github.com>
2023-11-01 16:50:03 -0600

    added api calling for model predictions

---
+++ js/fantasyApp.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("playerForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      // Get the form values
+      const playerName = document.getElementById("playerName").value;
+      const trimmedPlayerName = playerName.trim(); // Remove leading and trailing spaces
+      const playerNameWithPlus = trimmedPlayerName.replace(/ /g, "+"); // Replace spaces with '+'
+
+      const opponent = document.getElementById("opponent").value;
+      const week = document.getElementById("week").value;
+
+      // Construct the URL with query parameters
+      const apiUrl = `https://fantasy-api-daymonh.replit.app/?playerName=${playerNameWithPlus}&opponent=${opponent}&week=${week}`;
+
+      // Make a GET request to your API
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          document.getElementById(
+            "result"
+          ).innerHTML = `API Response: ${JSON.stringify(data)}`;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          document.getElementById("result").innerHTML =
+            "Error occurred while fetching data.";
+        });
+    });
+});
