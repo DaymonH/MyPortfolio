e3ead9592166e38c5226cbefcb3275b691956236
Daymon Hansen <83380479+DaymonH@users.noreply.github.com>
2023-11-02 15:31:57 -0600

    output change

--- js/fantasyApp.js
+++ js/fantasyApp.js
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((response) => response.json())
         .then((data) => {
           document.getElementById("result").style.display = "block"; // Show the result element
-          document.getElementById("result").innerHTML = `Predicted Points ${
-            JSON.stringify(data)[2]
-          }`;
+          document.getElementById(
+            "result"
+          ).innerHTML = `Predicted Points ${JSON.stringify(data)}`;
         })
         .catch((error) => {
           console.error("Error:", error);
