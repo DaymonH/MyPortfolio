2147397c4c973baa05f52830efde917a140d1164
Daymon Hansen <83380479+DaymonH@users.noreply.github.com>
2023-11-02 16:01:39 -0600

    change the way result is displayed

--- js/fantasyApp.js
+++ js/fantasyApp.js
@@ -16,13 +16,32 @@ document.addEventListener("DOMContentLoaded", function () {
       const apiUrl = `https://fantasy-api-daymonh.replit.app/?NAME=${playerNameWithPlus}&OPP=${opponent}&WEEK=${week}&@HOME=True`;
 
       // Make a GET request to your API
+      // fetch(apiUrl)
+      //   .then((response) => response.json())
+      //   .then((data) => {
+      //     document.getElementById("result").style.display = "block"; // Show the result element
+      //     document.getElementById(
+      //       "result"
+      //     ).innerHTML = `Predicted Points ${JSON.stringify(data)}`;
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error:", error);
+      //     document.getElementById("result").innerHTML =
+      //       "Error occurred while fetching data.";
+      //   });
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
-          document.getElementById("result").style.display = "block"; // Show the result element
-          document.getElementById(
-            "result"
-          ).innerHTML = `Predicted Points ${JSON.stringify(data)}`;
+          if (data && data.predicted_points !== undefined) {
+            const predictedPoints = data.predicted_points.toFixed(2); // Display up to 2 decimal places
+            document.getElementById("result").style.display = "block"; // Show the result element
+            document.getElementById(
+              "result"
+            ).innerHTML = `Predicted Points: ${predictedPoints}`;
+          } else {
+            document.getElementById("result").innerHTML =
+              "Invalid or missing data from the API.";
+          }
         })
         .catch((error) => {
           console.error("Error:", error);
